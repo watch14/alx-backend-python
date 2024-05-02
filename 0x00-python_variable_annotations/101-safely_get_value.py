a5d9d380cf3ea6e7b8dc15de1fa87a82929b85aa
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-02 23:13:39 +0100

    fast_push

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
-""" annotatuion """
-from typing import Mapping, Any, TypeVar, Union
+"""safely_get_value: Safely retrieve a value from a dictionary"""
+from typing import Mapping, Any, Union, TypeVar
+
 
 T = TypeVar('T')
 
 
 def safely_get_value(
-        dct: Mapping[Any, T], key: Any, default: Union[T, None] = None
-        ) -> Union[T, None]:
-    """annotation"""
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None] = None
+        ) -> Union[Any, T]:
+    """Safely retrieves a value from a dictionary."""
     if key in dct:
         return dct[key]
     else:
