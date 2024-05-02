26fb05f0cd51d08a31bf0de12ea71669de6d49ec
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-02 22:17:47 +0100

    fast_push

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ list of floats """
 
+from typing import List
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ sum list """
     return sum(input_list)
