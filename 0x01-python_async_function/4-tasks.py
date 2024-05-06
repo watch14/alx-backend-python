74ddb8d5bfe9000066b76ad4f1d76f9e9267bd37
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-06 10:10:04 +0100

    fast_push

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-""" async an async """
+""" asycnio """
 import asyncio
 from typing import List
-
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """ await async n times """
-        wait_times = await asyncio.gather(
+    """ asrt """
+    wait_times = await asyncio.gather(
         *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
     )
     return sorted(wait_times)
