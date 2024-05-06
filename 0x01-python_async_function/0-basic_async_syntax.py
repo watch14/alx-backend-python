9409b3283c680eb86b07aaae3a2fe71e9a82e4fa
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-06 10:13:53 +0100

    fast_push

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ random async """
     rando = random.random() * max_delay
     await asyncio.sleep(rando)
