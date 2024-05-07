31fdb5222cc4eddfdf8c4ec11283c7132d2f2bc3
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-07 05:18:52 +0100

    fast_push

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -6,4 +6,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
+    """ comper """
     return [x async for x in async_generator()]
