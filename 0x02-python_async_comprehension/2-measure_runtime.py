f5c1699492043b4051fc14a6e3828a55dbcafaf7
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-07 05:18:06 +0100

    fast_push

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
-
+""" mesure time """
 import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """ wierd randomness """
+    start = time.time()
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
+    end = time.time()
+    return end - start
