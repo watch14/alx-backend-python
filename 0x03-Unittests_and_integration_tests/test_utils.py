ddc02d22a8295959eeaaaf0955dafa42edadeca4
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-26 18:54:21 +0100

    fast_push

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """utils unit tests"""
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,6 +27,24 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), repr(path[-1]))
+
+
+class TestGetJson(unittest.TestCase):
+    """Test get_json function"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test get_json returns the expected result"""
+        with patch('utils.requests.get') as mock_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_payload
+            mock_get.return_value = mock_response
+
+            result = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
 
 
 if __name__ == '__main__':
