d3dd42c256ff0c0fcf4b4bc3a59ce353111cc562
watch14”git config --global user.name watch14” <watchman1414@gmail.com>
2024-05-26 19:07:26 +0100

    fast_push

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,18 +7,29 @@ from parameterized import parameterized
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ testing Github Org Client """
+    """Testing Github Org Client"""
 
     @parameterized.expand([
-        ('google'),
-        ('abc')
+        ('google',),
+        ('abc',),
     ])
     @patch('client.get_json')
-    def test_org(self, input, mock):
-        """GithubOrgClient.org test"""
+    def test_org(self, input, mock_get_json):
+        """Test org method of GithubOrgClient"""
         test_class = GithubOrgClient(input)
         test_class.org()
-        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+        expected_url = f'https://api.github.com/orgs/{input}'
+        mock_get_json.assert_called_once_with(expected_url)
+
+    @patch.object(GithubOrgClient, 'org')
+    def test_public_repos_url(self, mock_org):
+        """Test _public_repos_url property"""
+        expected_payload = {'public_repos': 10}
+        mock_org.return_value = expected_payload
+
+        test_class = GithubOrgClient('testorg')
+        expected_url = f'https://api.github.com/orgs/testorg/repos'
+        self.assertEqual(test_class._public_repos_url, expected_url)
 
 
 if __name__ == '__main__':
